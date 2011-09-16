ba6538e05fef5b1554c60413e940dfef89ca428a
Rowan Lewis <me@rowanlewis.com>
2011-09-17 03:26:25 +1000

    Added snicked and basic doccy autocompletion support.

--- extension.driver.php
+++ extension.driver.php
@@ -24,4 +24,21 @@
 				'description'	=> 'A text formatter for Doccy, the lightweight markup language for technical writing.'
 			);
 		}
+
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'initaliseAdminPageHead'
+				)
+			);
+		}
+
+		public function initaliseAdminPageHead($context) {
+			$page = Administration::instance()->Page;
+			$page->addScriptToHead(URL . '/extensions/doccy_text_formatter/snicked/assets/jquery.snicked.js');
+			$page->addScriptToHead(URL . '/extensions/doccy_text_formatter/snicked/assets/jquery.snickedCore.js');
+			$page->addScriptToHead(URL . '/extensions/doccy_text_formatter/assets/publish.js');
+		}
 	}
