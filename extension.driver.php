302adbb6d9c994e2ada94dfaca75d06b5f1747e7
Rowan Lewis <me@rowanlewis.com>
2012-03-25 01:02:37 +1000

    Prepared for release.

--- extension.driver.php
+++ extension.driver.php
@@ -8,23 +8,6 @@
 	 * A text formatter for Doccy, the lightweight markup language for technical writing.
 	 */
 	class Extension_Doccy_Text_Formatter extends Extension {
-		/**
-		 * Extension information.
-		 */
-		public function about() {
-			return array(
-				'name'			=> 'Text Formatter: Doccy',
-				'version'		=> '1.0',
-				'release-date'	=> '2011-09-14',
-				'author'		=> array(
-					'name'			=> 'Rowan Lewis',
-					'website'		=> 'http://rowanlewis.com/',
-					'email'			=> 'me@rowanlewis.com'
-				),
-				'description'	=> 'A text formatter for Doccy, the lightweight markup language for technical writing.'
-			);
-		}
-
 		public function getSubscribedDelegates() {
 			return array(
 				array(
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Doccy text formatter
+
+A text formatter for [Doccy](http://doccy.io/), the lightweight markup language for technical writing.
+
+
+## Installation from ZIP
+
+1. Upload the `doccy_text_formatter` folder in this archive to your Symphony `extensions` folder.
+2. Enable it by selecting the "Text Formatter: Doccy" extension, then choose "Enable" from the with-selected menu, and click apply.
+3. You can now choose "Doccy" as a text formatter in your field configuration.
