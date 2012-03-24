9e5b7fd38d34fd02a4bc1cebdca310076a2225fd
Rowan Lewis <me@rowanlewis.com>
2012-03-25 00:50:15 +1000

    Handle Doccy being used in duplicator instances.

--- assets/publish.js
+++ assets/publish.js
@@ -1,5 +1,19 @@
 (function($) {
 	$(document).ready(function() {
-		$('textarea.doccy').snickedDoccy();
+		$('textarea.doccy').each(function() {
+			var $textarea = $(this);
+
+			// It's in a duplicator template, wait to init:
+			if ($textarea.closest('li.template').length) {
+				$textarea.closest('div.frame')
+					.on('constructshow.duplicator', 'li.instance', function() {
+						$('textarea.doccy', this).snickedDoccy();
+					});
+			}
+
+			else {
+				$textarea.snickedDoccy();
+			}
+		});
 	});
 })(jQuery);
